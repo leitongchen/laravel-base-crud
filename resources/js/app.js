2deddef0cb16ea6c90465d28288a4fbaa5164686
leitong <leitongchen93@gmail.com>
2021-06-27 10:27:47 +0200

    js delete confirm, npm install

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
 require('./bootstrap');
+
+
+window.addEventListener('load', function () {
+    
+    const deleteForms = document.querySelectorAll('.delete-btn');
+
+    deleteForms.forEach(form => {
+
+        form.addEventListener('submit', (event) => {
+
+            if (!confirm('Are you sure you want to delete?')) {
+                event.preventDefault(); 
+            }
+
+        })
+    })
+})
